f634c9912353c60d9f0bc55e318c97a1857bb5ae
hach <nhat@hachr.com>
2014-05-06 22:49:05 -0700

    added oui.json and a way to parse the oui data

---
+++ tool/oui.js
@@ -0,0 +1,63 @@
+var fs = require('fs');
+var path = require('path');
+var EventEmitter = require('events').EventEmitter;
+
+var MAC_REGEX = /\s[0-9A-F]{2}-[0-9A-F]{2}-[0-9A-F]{2}(.*)/g;
+
+function Parser() {
+	this.item = null;
+	EventEmitter.call(this);
+}
+
+require('util').inherits(Parser, EventEmitter);
+
+Parser.prototype.process = function (line) {
+
+	if (MAC_REGEX.test(line)) {
+		if (this.item) {
+			this.emit('item', this.item);
+		}
+		this.item = {};
+		this.item['mac'] = line.slice(1, 10);
+		this.item['address'] = [];
+	} else if (this.item) {
+		if (this.item.name) {
+			this.item.address.push(line.slice(line.lastIndexOf('\t') + 1));
+		} else {
+			this.item.name = line.slice(line.lastIndexOf('\t') + 1);
+		}
+	}
+};
+
+Parser.prototype.end = function(){
+	this.emit('end');
+};
+
+
+
+var outfile = path.join(__dirname, 'oui.json');
+var parser = new Parser();
+var json = {};
+parser.on('item',function(item){
+	var mac = item.mac.replace(/-/g,'');
+	json[mac] = item;
+});
+
+parser.on('end', function(){
+	fs.writeFileSync(outfile, JSON.stringify(json, undefined, 2));
+});
+
+var file = path.join(__dirname, 'oui.txt');
+
+fs.readFile(file, function (err, data) {
+	if (!err) {
+		var split = data.toString().split("\r\n");
+		for (var i = 0; i < split.length; i++) {
+			var line = split[i];
+			if (line.length > 10) {
+				parser.process(line);
+			}
+		}
+		parser.end();
+	}
+});
