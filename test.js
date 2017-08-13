1c0d4e20957c28d42aab2b6aa18efe512ee7993f
hach <hach@fx160>
2017-08-12 22:54:44 -0700

    allow passing in cache value

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 var probe = require('./lib/probe');
-probe.probe(function(device, updated){
-    console.log( (updated ? "updated: " : "discovered: ") + device.mac + " - BSSID: " + device.bssid + " - " + device.probe);
+probe.probe({cacheDuration: 60000 * 5},function(device, updated){
+    console.log( (updated ? "updated: " : "discovered: ") + device.mac + " - BSSID: " + device.bssid + " - " + device.probe + " -  " + (device.oui ? device.oui.name : ""));
 });
